e11dbd6ea8a045e46670c0a20f5822cb420ac645
khalid bouychou <khalidbouychou22@gmail.com>
2023-08-19 16:36:11 +0100

    first program

---
+++ first.py
@@ -0,0 +1,23 @@
+# print("salam cv")
+# print("_" * 20)
+
+#***************** Variables *****************
+#lowercase letters --- > var should be lowercase and separated by _ (underscore) not started by number
+
+# price = 500;
+# #strings
+
+# name = """khalid cv hani lomor bouychou"""
+
+#***************** Variables *****************
+
+#***************** Escape Characters *****************
+# x = "khalid cv \"hani\" \\ \"lomor\" bouychou"
+# print(x)
+#******************* String format
+
+salam = "salam"
+cv = "cv"
+salamcv = f"{salam} {cv} {len(salam)}"
+
+print (salamcv.lstrip())
